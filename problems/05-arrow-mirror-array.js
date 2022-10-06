fa06dae650af6528b80aad115cbea48e0e37bf24
BaileyMagee <wolf217c@gmail.com>
2022-10-06 15:50:25 -0500

    05 solved

--- problems/05-arrow-mirror-array.js
+++ problems/05-arrow-mirror-array.js
@@ -9,8 +9,23 @@ Examples:
 arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
 arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 ***********************************************************************/
+const arrowMirrorArray = (array) => {
+  // let copy = array.slice()
 
+  // let copy2 = array.slice()
 
+  // let arr = [...copy, ...copy2.reverse()]
+
+  // return arr
+
+  let copy = array.slice(0)
+
+  return array.concat(copy.reverse())
+
+}
+
+console.log(arrowMirrorArray([1,2,3])); // => [ 1, 2, 3, 3, 2, 1 ]
+// arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
